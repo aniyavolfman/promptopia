534a36dfd7a08a86e203b6e07498b00063a7d800
Anna Volfman <aniya_07@ukr.net>
2023-06-09 14:43:01 +0300

    update

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-import Profile from "@components/profile";
+import Profile from "@components/Profile";
 import postcss from "postcss";
 import Spinner from "@components/Spinner";
 
