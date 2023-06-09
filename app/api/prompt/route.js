fe7da3031354b3a64313ff34149162196a443506
Anna Volfman <aniya_07@ukr.net>
2023-06-09 18:53:22 +0300

    update: update headers

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -3,10 +3,17 @@ import Prompt from "@models/prompt";
 
 export const GET = async (req, res) => {
   try {
-    await connectToDB()
+    await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
 
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+        "cache-control":
+          "private, no-cache, no-store, max-age=0, must-revalidate",
+      },
+    });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
